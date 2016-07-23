4ce0afc85919b8af7db8e8c6905f9c0ec7750964
shahz <shahz@DESKTOP-VF28LMH>
2016-07-22 21:26:23 -0400

    made the menu

--- Assignment5/Program.cs
+++ Assignment5/Program.cs
@@ -17,43 +17,76 @@ namespace Assignment5
         static void Main(string[] args)
         {
             Program student = new Program();
-
-            try
+            bool reservationmenu = true;
+            while (reservationmenu)
             {
-                string gradefile = "..\\..\\grade.txt";
-                
-                    const char dlim = ',';
-                    FileStream file = new FileStream(gradefile, FileMode.Open, FileAccess.Read);
-                    StreamReader filereader = new StreamReader(file);
-                    string recordFromfile;
-                    string[] fields;
+                // show options in the console menu
+                Console.WriteLine(" Please make your reservation");
+                Console.WriteLine(" 1. Display grades");
+                Console.WriteLine(" 2. Exit");
+                Console.WriteLine("----------------------------------");
+                string selection = Console.ReadLine();
+                switch (selection)
+                {
+                    case "1": // The "1" neumeric key selects displaygrades
+                        Console.WriteLine("please enter name of file to load");
+                        string filename = Console.ReadLine();
+                        if (filename == "grade.txt")
+                        {
+                            string gradefile = "..\\..\\grade.txt";
 
-                    recordFromfile = filereader.ReadLine();
-                    while (recordFromfile != null)
-                    {
-                        
-                        fields = recordFromfile.Split(dlim);
-                    student.lastname = fields[0];
-                    student.firstname = fields[1];
-                    student.idClass = fields[2];
-                    student.grade = fields[3];
+                            const char dlim = ',';
+                            FileStream file = new FileStream(gradefile, FileMode.Open, FileAccess.Read);
+                            StreamReader filereader = new StreamReader(file);
+                            string recordFromfile;
+                            string[] fields;
 
-                    Console.WriteLine("{0},{1},{2},{3}",student.lastname,student.firstname,student.idClass,student.grade);
+                            recordFromfile = filereader.ReadLine();
+                            while (recordFromfile != null)
+                            {
 
-                        recordFromfile = filereader.ReadLine();
-                    }
-                filereader.Close();
-                file.Close();
+                                fields = recordFromfile.Split(dlim);
+                                student.lastname = fields[0];
+                                student.firstname = fields[1];
+                                student.idClass = fields[2];
+                                student.grade = fields[3];
+
+                                Console.WriteLine("{0},{1},{2},{3}", student.lastname, student.firstname, student.idClass, student.grade);
+
+                                recordFromfile = filereader.ReadLine();
+                            }
+                            filereader.Close();
+                            file.Close();
+                        }
+                        else
+                        {
+                            Console.WriteLine("No such file exsists");
+                        }
+                        Console.WriteLine();
+                        break;
+                    case "2": // The "3" numeric Key to Exit the menu
+                        reservationmenu = false;
+                        break;
+                    default:
+                        Console.Clear();
+                        break;
+                }
+
+
+                try
+                {
+
+                }
+
+
+
+                catch (Exception exception)
+                {
+                    Console.WriteLine("error: " + exception.Message);
+                }
+
+
             }
-
-            
-
-            catch (Exception exception)
-            {
-                Console.WriteLine("error: " + exception.Message);
-            }
-            
-
+        }
     }
-}
 }
