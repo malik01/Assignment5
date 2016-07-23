5bd2df1fdc4f6db22f901924f4765365f72abe85
shahz <shahz@DESKTOP-VF28LMH>
2016-07-22 20:58:10 -0400

    made records in the text file and made fileds separated by comma and added appropriate things in the program class

--- Assignment5/Program.cs
+++ Assignment5/Program.cs
@@ -9,8 +9,49 @@ namespace Assignment5
 {
     class Program
     {
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public string idClass { get; set; }
+        public string grade { get; set; }
+
         static void Main(string[] args)
         {
-        }
+            Program student = new Program();
+
+            try
+            {
+                string gradefile = "..\\..\\grade.txt";
+                
+                    const char dlim = ',';
+                    FileStream file = new FileStream(gradefile, FileMode.Open, FileAccess.Read);
+                    StreamReader filereader = new StreamReader(file);
+                    string recordFromfile;
+                    string[] fields;
+
+                    recordFromfile = filereader.ReadLine();
+                    while (recordFromfile != null)
+                    {
+                        
+                        fields = recordFromfile.Split(dlim);
+                    student.lastname = fields[0];
+                    student.firstname = fields[1];
+                    student.idClass = fields[2];
+                    student.grade = fields[3];
+
+                        recordFromfile = filereader.ReadLine();
+                    }
+                filereader.Close();
+                file.Close();
+            }
+
+            
+
+            catch (Exception exception)
+            {
+                Console.WriteLine("error: " + exception.Message);
+            }
+            
+
     }
+}
 }
